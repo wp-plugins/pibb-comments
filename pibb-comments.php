2627a4a118815ca299d3114a1b52b1c11167830b
JanRain <JanRain@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-08-14 22:42:10 +0000

    First version of Pibb Comments
    git-svn-id: https://plugins.svn.wordpress.org/pibb-comments/trunk@16941 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pibb-comments.php
@@ -0,0 +1,149 @@
+<?php
+/*
+Plugin Name: Pibb Comments
+Plugin URI: https://pibb.com/plugins
+Description: Embedd a Pibb thread for commenting at the end of your blog posts.
+Author: Janrain, Inc.
+Author URI: http://www.janrain.com/
+Version: 0.1
+*/
+
+/*  Copyright 2007 Janrain, Inc. (email : support@janrain.com)
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
+
+function pibb_install () {
+   global $wpdb;
+   
+   $table_name = $wpdb->prefix . "pibbthreads";
+   if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+      $sql = "CREATE TABLE " . $table_name . " (
+	  id mediumint(9) NOT NULL AUTO_INCREMENT,
+	  post_id mediumint(9) NOT NULL,
+	  thread_id VARCHAR(55) NOT NULL,
+	  UNIQUE KEY id (id)
+	);";
+      dbDelta($sql);
+   }
+}
+
+function pibb_ensure_topic( $post_id ) {
+   global $wpdb;
+  
+   $table_name = $wpdb->prefix . "pibbthreads";
+   $thread_id = $wpdb->get_var("SELECT thread_id FROM $table_name where post_id='$post_id'");
+   if ($thread_id) {
+     return $post_id;
+   }
+   
+   $post = & get_post($post_id);
+
+   $pibb_channel_name = get_option('pibb_channel_name');
+   $pibb_api_key = get_option('pibb_api_key');
+
+   if ($pibb_channel_name && $pibb_api_key) {
+     /* Now we can fetch a url from the web */
+     $url = "https://pibb.com/pibb/embed_thread?channel_name=" .
+            urlencode($pibb_channel_name) . "&thread_name=" .
+            urlencode($post->post_title) . "&user_key=" .
+            urlencode($pibb_api_key);
+  
+     $cmdline = escapeshellcmd("/usr/bin/env curl --insecure " . $url);
+     //throw new Exception($cmdline);
+     exec($cmdline, $results, $return);
+     if(!$return) {
+        $insert = "INSERT INTO " . $table_name .
+                  " (post_id, thread_id) " .
+                  "VALUES ('" . $post_id . "','" . $wpdb->escape($results[0]) . "')";
+  
+        $results = $wpdb->query( $insert );
+     } else {
+       throw new Exception("curl result code [" . $return . "] [" . $sesults . "]");
+     }
+   }
+   
+   return $post_id;
+}
+
+function pibbify_content( $content ) {
+    global $post;
+    global $wpdb;
+
+    // if not in entry context, no link, bail
+    if ( ! is_single() ) {
+        return $content;
+    }
+
+    $table_name = $wpdb->prefix . "pibbthreads";
+    $post_id = $post->ID;
+    $thread_id = $wpdb->get_var("SELECT thread_id FROM $table_name where post_id='$post_id'");
+    if ($thread_id) {
+      $suffix = "<div style=\"height: 400px;\" id=\"__pibb_thread\"></div><script src=\"https://pibb.com/widget/thread/" . urlencode($thread_id) . "\" type=\"text/javascript\"></script>";
+      $content.= $suffix;
+    }
+
+    return $content;
+}
+
+add_action('activate_pibb-comments.php', 'pibb_install');
+add_action('publish_post', 'pibb_ensure_topic');
+add_action('the_content','pibbify_content');
+add_action('admin_menu', 'pibb_rc_config_page');
+
+function pibb_rc_config_page() {
+  global $wpdb;
+  if ( function_exists('add_submenu_page') )
+    add_submenu_page('plugins.php', __('Pibb Configuration'), __('Pibb Configuration'),
+                     'manage_options', __FILE__, 'pibb_rc_conf');
+}
+
+
+function pibb_rc_conf() {
+  if ( isset($_POST['src_frmsubmit']) ) {
+    if ( !current_user_can('manage_options'))
+      die(__('Cheatin&#8217; uh?'));
+      
+    $pibb_channel_name = trim($_POST['pibb_channel_name']);
+    $pibb_api_key = trim($_POST['pibb_api_key']);
+
+    update_option('pibb_channel_name', $pibb_channel_name);
+    update_option('pibb_api_key', $pibb_api_key);
+  } else {
+    $pibb_channel_name = get_option('pibb_channel_name');
+    $pibb_api_key = get_option('pibb_api_key');
+ }
+?>
+<div class="wrap">
+<h2><?php _e('Pibb Comments Configuration'); ?></h2>
+<form action="" method="post" style="margin: auto; width: 25em; ">
+<input type="hidden" name="src_frmsubmit" value="1"/>
+<h3><label>Pibb Channel Name:</label></h3>
+<p>
+  <input type="text" name="pibb_channel_name" value="<?php echo $pibb_channel_name; ?>"/>
+</p>
+<h3><label>Pibb API Key:</label></h3>
+<p>
+  <input type="text" name="pibb_api_key" value="<?php echo $pibb_api_key; ?>"/>
+</p>
+<p>
+  <input type="submit"/>
+</p>
+</form>
+</div>
+<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Pibb Comments ===
+Tags: Comments, Pibb, Chat, Realtime
+Contributors: JanRain Inc, Grant Monroe
+
+The Pibb Comments plug-in displays an embedded Pibb Thread at the bottom of each new blog post to allow realtime discussion of the post.
+
+A user will need to login to Pibb (with an OpenID account), before they can make a comment.  
+
+An OpenID accountis free, quick and easy to get from http://myopenid.com
+
+== Installation ==
+
+1. Copy the file pibb-comments.php into your plugins directory (wp-content/plugins/).
+
+2. Login to your WordPress Admin Panel 
+
+3. Go to the Plugins tab
+
+4. Activate the 'Pibb Comments' plugin. (Click Activate in the right column).
+
+5. You will now need to login to Pibb (https://pibb.com) and create a channel. 
+   This channel (e.g. JanRain Blog) is where all of the conversation threads
+   for your posts will be stored. One channel can have many threads.
+
+6. You will also need to get an API key, the API key can be found by clicking 
+   on the 'My Profile' tab which (once you are logged in)  is in the upper 
+   right hand corner of Pibb.   
+
+7. Now associate your blog with your Pibb Channel (e.g. JanRain Blog) and API key. 
+   To do this login to the admin panel and click on the "Plugins" menu and 
+   then click on the "Pibb Configuration" sub-menu to enter your Pibb Channel 
+   Name and API key.
+
+8. That's it, go ahead and make a post and when you publish your post you will 
+   have a Pibb thread embedded at the bottom, ready for realtime discussion
+   of your post. Please note the Pibb Comments thread will not show up until you
+   publish your post.
+
+== Requirements ==
+
+1. Your WordPress theme must contain a call to the the_content() function
+
+== Support ==
+
+Please login to Pibb and visit our WordPress feedback channel:
+https://pibb.com/go/WordPressPlugin/feedback
+
+OR
+
+You can leave us feedback by going here:
+https://pibb.com/feedback
+
+
+
